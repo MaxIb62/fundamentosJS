d7c8aa5bf028c592ffceefbd1e46670c2fe9781b
MaxIb62 <cdmx2254@amerike.edu.mx>
2023-09-27 14:09:26 -0600

    javascript

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# fundamentosJS
+# Maximiliano Ibarra Rojas
+
+## parcial 2 - practica 4
+
+[link]()
---
+++ fundamentosjavascript/fundamentos.js
@@ -0,0 +1,69 @@
+
+  
+  NOMBRES = "Tonatuih Maximiliano",
+  apellidos = "Ibarra Rojas";
+    edad = 22
+    pasatiempo = " tomar fotografias y escuchar musica"
+    peliculas = "transformers, Cars y Gran turismo "
+ 
+
+
+let yo =
+  "Hola, me llamo, " +
+  NOMBRES +
+  " " +
+  apellidos +
+  ", y tengo " +
+  edad +
+  " años, en mi tiempo libre me gusta" +
+  pasatiempo +
+
+  ""
+
+console.log(yo);
+
+let transformersElultimoCaballero = {
+    pelicula: "transformers: ElultimoCaballero",
+    director: "Michael Bay",
+    genero: "ciencia ficcion",
+    pais: "Estadus Unidos",
+    anio: 2006,
+    duracion: {
+      munitos: 120,
+    },
+  };
+  
+  console.log(transformersElultimoCaballero);
+
+
+  let Cars = {
+    pelicula:"cars",
+    directores: "John Lasseter y Joe Ranft",
+    genero: "Animacion, Comedia, Aventura",
+    pais: "Estadus Unidos",
+    anio: 2006,
+    duracion: {
+      munitos: 116,
+    },
+  };
+  
+  console.log(Cars);
+
+  let GranTurismo = {
+    pelicula:"Gran Turismo",
+    directores: "Neill Blomkamp",
+    genero: "Deportes, Drama, Accion",
+    pais: "Estadus Unidos",
+    anio: 2023,
+    duracion: {
+      munitos: 134,
+    },
+  };
+  
+  console.log(GranTurismo);
+
+  
+  
+
+  
+
